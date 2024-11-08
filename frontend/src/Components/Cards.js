6a1fd3683a10b0d56f65a7e5c82bffd10f7de163
Danyliuk04 <ulianjjjjhj@gmail.com>
2024-11-08 19:00:38 +0200

    Changed navigation points, Added Activity and leaderboard Pages, created DateFilter

---
+++ frontend/src/Components/Cards.js
@@ -0,0 +1,36 @@
+import { Card, CardImg, Col, Row, Container, CardBody, CardText, CardHeader } from "react-bootstrap";
+import student from '../Images/student.png'
+import teacher from '../Images/teacher.png'
+const Cards = () => {
+    return ( <Container style={{paddingTop: '2rem', paddingBottom:'2rem'}}>
+        <Row>
+            <Col>
+                <Card style={{width: '20rem', height:'600px'}}>
+                <CardHeader>Students portal</CardHeader>
+                    <CardImg variant="top" src={student}  />
+                    <CardBody>
+                        
+                        <CardText>
+                        Access a wide range of interactive modules designed to enhance your software engineering skills. Work through real-world tasks, track your progress, and improve your coding proficiency with detailed analytics and feedback.
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+        <Col>
+            <Card style={{width: '20rem', height:'600px'}}>
+            <CardHeader>Editor portal</CardHeader>
+                <CardImg variant="top" src={teacher}  />
+                <CardBody>
+                    <CardText>
+                    Manage and customize course content, monitor student progress, and provide detailed feedback. Use analytics to assess performance and fine-tune tasks to ensure students meet their learning goals.
+                    </CardText>
+                </CardBody>
+            </Card>
+            
+            </Col>
+            </Row>
+
+    </Container> );
+}
+ 
+export default Cards;
---
+++ frontend/src/Pages/HomePage.js
@@ -0,0 +1,16 @@
+import Cards from "../Components/Cards";
+import Slider from "../Components/Slider";
+
+const HomePage = () => {
+    return ( 
+    <>
+    <Slider/>
+    <div className="cards-info">
+        <h1>The service is created for: </h1>
+        <Cards/>
+    </div>
+    </>
+     );
+}
+ 
+export default HomePage;
