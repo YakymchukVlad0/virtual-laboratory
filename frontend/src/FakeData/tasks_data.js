5b162887a69994a78c221d784c4e37f3a59b4018
VladYakymchuk17 <vladyakimchuk005@gmail.com>
2024-11-29 20:09:36 +0200

    Added sort, filter options. Changed diagrams and table view and logic.

---
+++ frontend/src/FakeData/tasks_data.js
@@ -0,0 +1,474 @@
+const newTasks = [
+    {
+        "Code": "No code",
+        "TasksCompleted": 6,
+        "Attempts": 2,
+        "Errors": 14,
+        "PercentageScore": "30.0%",
+        "TimeSpent": "3.6",
+        "Deadline": "11.11.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "0.60",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "advanced",
+        "CodeLength": 1330,
+        "ErrorTypes": [
+            "logical",
+            "syntax",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 14,
+        "Attempts": 3,
+        "Errors": 6,
+        "PercentageScore": "70.0%",
+        "TimeSpent": "8.1",
+        "Deadline": "20.11.2024",
+        "ProgrammingLanguage": "Java",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "0.58",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "advanced",
+        "CodeLength": 1991,
+        "ErrorTypes": [
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 13,
+        "Attempts": 1,
+        "Errors": 7,
+        "PercentageScore": "65.0%",
+        "TimeSpent": "13.1",
+        "Deadline": "13.10.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "1.01",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1014,
+        "ErrorTypes": [
+            "syntax",
+            "logical",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 12,
+        "Attempts": 1,
+        "Errors": 8,
+        "PercentageScore": "60.0%",
+        "TimeSpent": "12.3",
+        "Deadline": "23.10.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "1.03",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "advanced",
+        "CodeLength": 1370,
+        "ErrorTypes": [
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 14,
+        "Attempts": 2,
+        "Errors": 6,
+        "PercentageScore": "70.0%",
+        "TimeSpent": "12.6",
+        "Deadline": "31.10.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "0.90",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "advanced",
+        "CodeLength": 1729,
+        "ErrorTypes": [
+            "syntax",
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 19,
+        "Attempts": 1,
+        "Errors": 1,
+        "PercentageScore": "95.0%",
+        "TimeSpent": "13.1",
+        "Deadline": "23.11.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "0.69",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "advanced",
+        "CodeLength": 2025,
+        "ErrorTypes": [
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 2,
+        "Attempts": 1,
+        "Errors": 18,
+        "PercentageScore": "10.0%",
+        "TimeSpent": "14.3",
+        "Deadline": "20.10.2024",
+        "ProgrammingLanguage": "C++",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "7.15",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "beginner",
+        "CodeLength": 432,
+        "ErrorTypes": [
+            "logical",
+            "syntax",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 10,
+        "Attempts": 2,
+        "Errors": 10,
+        "PercentageScore": "50.0%",
+        "TimeSpent": "12.3",
+        "Deadline": "17.09.2024",
+        "ProgrammingLanguage": "C",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "1.23",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1172,
+        "ErrorTypes": [
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 6,
+        "Attempts": 1,
+        "Errors": 14,
+        "PercentageScore": "30.0%",
+        "TimeSpent": "14.7",
+        "Deadline": "19.11.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "2.45",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "advanced",
+        "CodeLength": 1926,
+        "ErrorTypes": [
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 10,
+        "Attempts": 2,
+        "Errors": 10,
+        "PercentageScore": "50.0%",
+        "TimeSpent": "12.2",
+        "Deadline": "10.11.2024",
+        "ProgrammingLanguage": "C",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "1.22",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "advanced",
+        "CodeLength": 1366,
+        "ErrorTypes": [
+            "logical",
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 1,
+        "Attempts": 2,
+        "Errors": 19,
+        "PercentageScore": "5.0%",
+        "TimeSpent": "3.9",
+        "Deadline": "28.10.2024",
+        "ProgrammingLanguage": "C",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "3.90",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "beginner",
+        "CodeLength": 289,
+        "ErrorTypes": [
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 19,
+        "Attempts": 2,
+        "Errors": 1,
+        "PercentageScore": "95.0%",
+        "TimeSpent": "11.6",
+        "Deadline": "30.09.2024",
+        "ProgrammingLanguage": "Java",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "0.61",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1060,
+        "ErrorTypes": [
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 14,
+        "Attempts": 2,
+        "Errors": 6,
+        "PercentageScore": "70.0%",
+        "TimeSpent": "2.2",
+        "Deadline": "04.10.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "0.16",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 877,
+        "ErrorTypes": [
+            "runtime",
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 19,
+        "Attempts": 3,
+        "Errors": 1,
+        "PercentageScore": "95.0%",
+        "TimeSpent": "10.6",
+        "Deadline": "25.11.2024",
+        "ProgrammingLanguage": "C",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "0.56",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "advanced",
+        "CodeLength": 1905,
+        "ErrorTypes": [
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 8,
+        "Attempts": 1,
+        "Errors": 12,
+        "PercentageScore": "40.0%",
+        "TimeSpent": "7.4",
+        "Deadline": "20.11.2024",
+        "ProgrammingLanguage": "C++",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "0.93",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "advanced",
+        "CodeLength": 2064,
+        "ErrorTypes": [
+            "syntax",
+            "runtime",
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 16,
+        "Attempts": 2,
+        "Errors": 4,
+        "PercentageScore": "80.0%",
+        "TimeSpent": "4.7",
+        "Deadline": "11.10.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "0.29",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 852,
+        "ErrorTypes": [
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 7,
+        "Attempts": 3,
+        "Errors": 13,
+        "PercentageScore": "35.0%",
+        "TimeSpent": "12.6",
+        "Deadline": "25.09.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "loops",
+        "AverageCompletionTime": "1.80",
+        "DeadlineStatus": "early submission",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1159,
+        "ErrorTypes": [
+            "logical",
+            "syntax",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 4,
+        "Attempts": 3,
+        "Errors": 16,
+        "PercentageScore": "20.0%",
+        "TimeSpent": "7.4",
+        "Deadline": "28.10.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "1.85",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "beginner",
+        "CodeLength": 408,
+        "ErrorTypes": [
+            "runtime",
+            "syntax",
+            "logical"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 4,
+        "Attempts": 3,
+        "Errors": 16,
+        "PercentageScore": "20.0%",
+        "TimeSpent": "3.9",
+        "Deadline": "17.09.2024",
+        "ProgrammingLanguage": "C++",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "0.97",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "beginner",
+        "CodeLength": 255,
+        "ErrorTypes": [
+            "logical",
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 7,
+        "Attempts": 2,
+        "Errors": 13,
+        "PercentageScore": "35.0%",
+        "TimeSpent": "3.6",
+        "Deadline": "21.10.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "0.51",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1237,
+        "ErrorTypes": [
+            "logical",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 3,
+        "Attempts": 1,
+        "Errors": 17,
+        "PercentageScore": "15.0%",
+        "TimeSpent": "11.0",
+        "Deadline": "23.09.2024",
+        "ProgrammingLanguage": "C",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "3.67",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "beginner",
+        "CodeLength": 527,
+        "ErrorTypes": [
+            "syntax"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 7,
+        "Attempts": 3,
+        "Errors": 13,
+        "PercentageScore": "35.0%",
+        "TimeSpent": "5.0",
+        "Deadline": "25.11.2024",
+        "ProgrammingLanguage": "Java",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "0.71",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "advanced",
+        "CodeLength": 1697,
+        "ErrorTypes": [
+            "syntax",
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 17,
+        "Attempts": 1,
+        "Errors": 3,
+        "PercentageScore": "85.0%",
+        "TimeSpent": "4.5",
+        "Deadline": "12.10.2024",
+        "ProgrammingLanguage": "Python",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "0.26",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 789,
+        "ErrorTypes": [
+            "runtime"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 20,
+        "Attempts": 1,
+        "Errors": 0,
+        "PercentageScore": "100.0%",
+        "TimeSpent": "5.6",
+        "Deadline": "13.10.2024",
+        "ProgrammingLanguage": "C#",
+        "TaskCategory": "file handling",
+        "AverageCompletionTime": "0.28",
+        "DeadlineStatus": "missed deadline",
+        "SkillLevel": "intermediate",
+        "CodeLength": 1129,
+        "ErrorTypes": [
+            "No error"
+        ]
+    },
+    {
+        "Code": "No code",
+        "TasksCompleted": 6,
+        "Attempts": 3,
+        "Errors": 14,
+        "PercentageScore": "30.0%",
+        "TimeSpent": "6.4",
+        "Deadline": "20.09.2024",
+        "ProgrammingLanguage": "Java",
+        "TaskCategory": "recursion",
+        "AverageCompletionTime": "1.07",
+        "DeadlineStatus": "on time",
+        "SkillLevel": "intermediate",
+        "CodeLength": 659,
+        "ErrorTypes": [
+            "syntax",
+            "runtime",
+            "logical"
+        ]
+    }
+]
+
+export default newTasks;
